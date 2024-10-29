dbfd54ae699a0d8ef03e1ec8dbe8de2737046760
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-10-29 12:21:58 +0100

    modify iterator

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -49,7 +49,7 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
             return wizPos < size;
         }
         public T next() {
-            T returnItem = items[wizPos];
+            T returnItem = get(wizPos);
             wizPos += 1;
             return returnItem;
         }
