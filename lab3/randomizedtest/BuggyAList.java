c6b89805fc3b41f2652dce457b0092378e199533
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-11-07 22:00:25 +0100

    finish lab03: Timing Tests and Randomized Comparison Tests

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -59,12 +59,13 @@ public class BuggyAList<Item> {
     /** Deletes item from back of the list and
       * returns deleted item. */
     public Item removeLast() {
-        Item x = getLast();
-        items[size - 1] = null;
-        size = size - 1;
         if ((size < items.length / 4) && (size > 4)) {
             resize(items.length / 4);
         }
+        Item x = getLast();
+        items[size - 1] = null;
+        size = size - 1;
+
         return x;
     }
 }
